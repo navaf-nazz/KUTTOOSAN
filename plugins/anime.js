fd037834a43465020764c6e6116269f0b93c1d8e
DEVILSER <88338865+DEVILSER@users.noreply.github.com>
2021-08-24 13:30:58 +0000

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -96,7 +96,7 @@ if (Config.WORKTYPE == 'private') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by PLK'})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by *KUTTOOSAN* '})
 
     }));
 }
@@ -190,7 +190,7 @@ else if (Config.WORKTYPE == 'public') {
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: Config.AFN})
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: 'Made by *KUTTOOSAN* '})
 
     }));
 }
